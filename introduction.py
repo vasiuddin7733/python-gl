e9d2318aa610b361df2869926d56184fb2ec39be
vasiuddin <vasiuddin.mohammed1@gmail.com>
2025-02-02 11:06:14 +0100

    add intro file

---
+++ introduction.py
@@ -0,0 +1,39 @@
+
+#  Introduction
+
+print("Hello, I am vasiuddin mohammed")
+
+'''
+this is multi line comments
+'''
+
+a = 1
+b = "apple"
+
+print(a,b)
+
+# Varible
+c = 1232
+d = 0.1235
+e = False
+
+print(type(c),type(d),type(e))
+
+print(True == 1)
+print(False == 0)
+
+# Conditional Operators
+age = 25
+if age>18:
+ print("you are voter")
+elif age<18:
+    print("await")
+
+# Looging Statements
+g = [1,2,3,4,5,6,7,8,9,10]
+
+for i in g:
+    print(i+1)
+
+
+
